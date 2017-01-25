444df416e9208c69ef9587dfe4ae5ac3506f645f
Kes <keskpeart@gmail.com>
2017-01-24 22:41:02 -0500

    added first() and implemented interface

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {BaseComponent} from '../common/base.component';
 import {SearchAPIService} from './search.api.service';
@@ -8,7 +8,7 @@ import {SearchAPIService} from './search.api.service';
     templateUrl: './search.component.html',
     styleUrls: ['./search.component.css']
 })
-export class SearchComponent extends BaseComponent {
+export class SearchComponent extends BaseComponent implements OnInit{
      searchFrom: FormGroup;
      predictions: Array<any>;
 constructor(private searchAPI: SearchAPIService, private formBuilder: FormBuilder){
@@ -25,6 +25,7 @@ constructor(private searchAPI: SearchAPIService, private formBuilder: FormBuilde
                 .subscribe((changes: {search: string}) => {
                     this.searchAPI
                         .searchPredictions(changes.search)
+                        .first()
                         .subscribe((searchPredictions: any) => {
                             this.predictions = searchPredictions;
                         });
