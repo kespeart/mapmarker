33cf6f48e1f9fdef59fd7643d1858b865178792c
Kes <keskpeart@gmail.com>
2017-02-11 01:35:29 -0500

    remove un-used server file; css and gulp file update

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,34 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var gulpSequence = require('gulp-sequence');
 var webpackConfig = require('./config/webpack.prod.js');
-var server = require('gulp-express');
+var gls = require('gulp-live-server');
 var del = require('del');
 
 gulp.task('build', webpackBuild);
 
-gulp.task('server', ()=> server.run(['./server/server.prod.js'], null, false));
-gulp.task('clean', cleanTask);
-gulp.task('dev', ()=> server.run(['./server/server.dev.js'], null, false));
-gulp.task('prod', gulpSequence('clean', 'build', 'server'));
+gulp.task('prod-server', prod);
+gulp.task('clean', () => del(['dist/']));
+gulp.task('prod', gulpSequence('clean', 'build', 'prod-server'));
+gulp.task('dev', dev);
 
+/**
+ * @desc dev server function
+ */
+function dev(){
+     var server = gls.new('./server/server.dev.js');
+     server.start();
+    gulp.watch(['src/**/*.*', 'src/*.*'], function (file) {
+        server.notify.apply(server, [file]);
+    });
+}
 
+/**
+ * @desc function to start production server
+ */
+function prod(){
+    var server = gls.new('./server/server.prod.js');
+    server.start();
+}
 /**
 *@desc run webpack build which will create dist folder
 */
@@ -20,9 +37,4 @@ function webpackBuild(){
     return gulp.src('src/main.ts')
              .pipe(webpack(webpackConfig))
              .pipe(gulp.dest('dist/'));
-}
-
-
-function cleanTask(){
-    del(['dist/'])
 }
